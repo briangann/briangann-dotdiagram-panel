c9e143fd5ae3e99e6fe5f3ca48d3499feb2de644
Brian Gann <bkgann@gmail.com>
2024-03-19 00:20:59 -0400

    update

--- playwright/post-auth/panel/panel-create.spec.ts
+++ playwright/post-auth/panel/panel-create.spec.ts
@@ -4,9 +4,12 @@ test('check grafana version', async ({ grafanaVersion }) => {
   expect(grafanaVersion).toEqual("10.3.3");
 });
 
-test('data query should return polystat with label A-series', async ({ panelEditPage }) => {
+/*
+Skip - how do we defined visualization types?\
+
+test('data query should return dotdiagram with label A-series', async ({ panelEditPage }) => {
   await panelEditPage.datasource.set('gdev-testdata');
-  await panelEditPage.setVisualization('Polystat');
+  await panelEditPage.setVisualization('briangann-dotdiagram-panel');
   // first render will show OK in the display
   await expect(panelEditPage.panel.locator.getByText('OK')).toBeVisible();
   await expect(panelEditPage.refreshPanel()).toBeOK();
@@ -14,3 +17,4 @@ test('data query should return polystat with label A-series', async ({ panelEdit
   await expect(panelEditPage.refreshPanel()).toBeOK();
   await expect(panelEditPage.panel.locator.getByText('A-series')).toBeVisible();
 });
+*/
